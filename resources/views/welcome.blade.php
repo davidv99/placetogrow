3711acc4abe2ca439d4a57a1ad3d7d2a7c98ac79
Brayan Lujan Munoz <brayan.lujan@evertecinc.com>
2024-06-05 22:54:56 -0500

    feat: welcome page created
    modify the routes, created the controller, put the image for the logo, create the welcome view and the breeze files

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f8fafc;
+        }
+        .welcome-container {
+            text-align: center;
+        }
+        .welcome-image {
+            max-width: 100%;
+            height: auto;
+            padding: 10px 20px;
+            border-radius: 10px;
+            margin-bottom: 40px;
+        }
+        .login-button {
+            margin-top: 20px;
+            padding: 10px 20px;
+            background-color: #702bd8;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            text-decoration: none;
+        }
+        .login-button:hover {
+            background-color: #16a5b2;
+        }
+    </style>
+</head>
+<body>
+    <div class="welcome-container">
+        <h1>Welcome to PlaceToGrow Microsites manager</h1>
+        <img src="{{ asset('images/welcome.jpg') }}" alt="Welcome Image" class="welcome-image">
+        <br>
+        <a href="{{ route('login') }}" class="login-button">Login</a>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
+Route::get('/', WelcomeController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
