897f0b18d261364ed8ee1c99a946387d7f600836
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-13 17:53:42 -0500

    feat: Login void schema

--- app/Http/Controllers/Auth/RegisterUserController.php
+++ app/Http/Controllers/Auth/RegisterUserController.php
@@ -35,27 +35,4 @@ class RegisterUserController extends Controller
         return response()->json(compact('user', 'token'), 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
