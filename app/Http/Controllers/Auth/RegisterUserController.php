832cc7773621b6c669a5ed9ca584c1bef6db7391
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-13 14:07:40 -0500

    feat: void schema to register an user

--- app/Http/Controllers/Auth/RegisterUserController.php
+++ app/Http/Controllers/Auth/RegisterUserController.php
@@ -5,16 +5,14 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AuthController extends Controller
+class RegisterUserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return response()->json([
-            'mesagge' => 'hello user'
-        ], 200);
+        //
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::prefix('auth')-> group(function(){
+    Route::get('/', [AuthController::class, 'index']);
+});
