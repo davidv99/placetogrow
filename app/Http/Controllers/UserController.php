bda7a27032460d63cafb35f1a3e521379164668d
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-17 17:08:13 -0500

    feat: simple user controller schema

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json(compact('users'),200);
+    }
+
+    public function show(string $id)
+    {
+
+    }
+
+    public function update(Request $request, string $id)
+    {
+
+    }
+
+    public function destroy(string $id)
+    {
+        
+    }
+}
