13c8331f07539d45e235488a899436d057df1201
Brayan Lujan Munoz <brayan.lujan@evertecinc.com>
2024-06-18 16:06:00 -0500

    feat: user configuration
    restringed guest user access and use permissions
    show the profile to the user logged

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -16,6 +16,17 @@ class PermissionSeeder extends Seeder
         $admin_role = Role::where('name', 'admin')->first();
         $guest_role = Role::where('name', 'guest')->first();
 
+        Permission::create(['name' => 'users_menu.show'])->syncRoles([$super_admin_role, $admin_role]);
+        Permission::create(['name' => 'super_user.options'])->syncRoles([$super_admin_role]);
+
+        Permission::create(['name' => 'user.create'])->syncRoles([$super_admin_role, $admin_role]);
         Permission::create(['name' => 'user.edit'])->syncRoles([$super_admin_role, $admin_role]);
+        Permission::create(['name' => 'user.show'])->syncRoles([$super_admin_role, $admin_role, $guest_role]);
+        Permission::create(['name' => 'user.destroy'])->syncRoles([$super_admin_role, $admin_role]);
+
+        Permission::create(['name' => 'super_user.create'])->syncRoles([$super_admin_role]);
+        Permission::create(['name' => 'super_user.edit'])->syncRoles([$super_admin_role]);
+        Permission::create(['name' => 'super_user.show'])->syncRoles([$super_admin_role, $admin_role]);
+        Permission::create(['name' => 'super_user.destroy'])->syncRoles([$super_admin_role]);
     }
 }
