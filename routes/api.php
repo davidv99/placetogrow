d936b2f343fea065b76e3aaa50b74d52e97ad408
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2024-06-18 20:30:06 -0500

    feat: user controller index schema to test authentication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,5 +12,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')-> group(function () {
     Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);//->middleware(JwtMiddleware::class);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/users', [UserController::class,  'index'])->middleware(JwtMiddleware::class);
 });
