eac954cec7d4e326bb6c9b6bcccbd9906d2da523
Cristian Castano <cristiancastano852@gmail.com>
2024-06-28 20:16:18 -0500

    fix: change command for execute test

--- tests/Feature/MicrositesEditTest.php
+++ tests/Feature/MicrositesEditTest.php
@@ -7,7 +7,6 @@ use App\Models\Category;
 use App\Models\Microsites;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Spatie\Permission\Models\Permission;
 use Tests\TestCase;
 
